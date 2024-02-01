928c2ca886cac65df93502752880d573b844afac
Heber Rossi <137964988+heberrossi@users.noreply.github.com>
2024-02-01 14:03:47 -0300

    Add files via upload

---
+++ MÓD 15 - STREAMLIT/streamlit - jupyter notebook/gerar analise.py
@@ -0,0 +1,64 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+sns.set()
+
+def plota_pivot_table(df, value, index, func, ylabel, xlabel, opcao='nada'):
+    if opcao == 'nada':
+        pd.pivot_table(df, values=value, index=index,
+                       aggfunc=func).plot(figsize=[15, 5])
+    elif opcao == 'sort':
+        pd.pivot_table(df, values=value, index=index,
+                       aggfunc=func).sort_values(value).plot(figsize=[15, 5])
+    elif opcao == 'unstack':
+        pd.pivot_table(df, values=value, index=index,
+                       aggfunc=func).unstack().plot(figsize=[15, 5])
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    st.pyplot(fig=plt)
+    return None
+
+st.set_page_config(page_title='SINASC Rondônia', 
+                   page_icon= 'https://image.shutterstock.com/image-photo/image-260nw-2141654667.jpg' , 
+                   layout='wide')
+
+st.write("# Análise SINASC")
+
+sinasc = pd.read_csv('./input/SINASC_RO_2019.csv')
+
+sinasc.DTNASC = pd.to_datetime(sinasc.DTNASC)
+
+min_data = sinasc.DTNASC.min()
+max_data = sinasc.DTNASC.max()
+
+st.write(min_data)
+st.write(max_data)
+
+
+datas = pd.DataFrame(sinasc.DTNASC.unique(), columns=['DTNASC'])
+datas.sort_values(by='DTNASC', inplace=True, ignore_index=True)
+st.write(datas)
+
+data_inicial = st.sidebar.date_input("Data inicial",
+                              value=min_data, 
+                              min_value=min_data, 
+                              max_value=max_data)
+data_final = st.sidebar.date_input("Data final",
+                              value=max_data, 
+                              min_value=min_data, 
+                              max_value=max_data)
+
+st.write('Data inical = ', data_inicial)
+st.write('Data final = ', data_final)
+
+teste = sinasc[(sinasc['DTNASC'] <= pd.to_datetime(data_final)) & (sinasc['DTNASC'] >= pd.to_datetime(data_inicial))] 
+st.write(teste)
+
+plota_pivot_table(sinasc, 'IDADEMAE', 'DTNASC', 'count', 'quantidade de nascimento','data de nascimento')
+plota_pivot_table(sinasc, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean', 'media idade mae','data de nascimento','unstack')
+plota_pivot_table(sinasc, 'PESO', ['DTNASC', 'SEXO'], 'mean', 'media peso bebe','data de nascimento','unstack')
+plota_pivot_table(sinasc, 'PESO', 'ESCMAE', 'median', 'apgar1 mediano','gestacao','sort')
+plota_pivot_table(sinasc, 'APGAR1', 'GESTACAO', 'mean', 'apgar1 medio','gestacao','sort')
+plota_pivot_table(sinasc, 'APGAR5', 'GESTACAO', 'mean', 'apgar5 medio','gestacao','sort')
